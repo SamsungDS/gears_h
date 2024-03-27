a450054153a11da03564776a979c0c9655a5ac16
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-03-27 10:57:38 -0700

    First draft of atom-centered descriptor works, no neighbour contractions yet

--- surrogatelcaohamiltonians/layers/descriptor/atomcentered.py
+++ surrogatelcaohamiltonians/layers/descriptor/atomcentered.py
@@ -13,22 +13,29 @@ e3x.Config.set_cartesian_order(False)
 
 
 class AtomCenteredTensorMomentDescriptor(nn.Module):
-  radial_function: e3x.nn.sinc
   cutoff: float
+  num_basis_features: int = 8
   basis_max_degree: int = 3
+  num_moment_features: int = 64 # TODO this can in principle be a list of ints
   moment_max_degree: int = 4
-  max_moment: 3
+  max_moment: int = 2
+  tensor_module: nn.Module = e3x.nn.TensorDense
 
   def setup(self):
-    pass
+    self.radial_function = partial(e3x.nn.sinc, limit=self.cutoff)
 
-  def __call__(self, atomic_numbers, neighbour_displacements, neighbour_indices):
+  @nn.compact
+  def __call__(self, neighbour_displacements):
 
     y = e3x.nn.basis(neighbour_displacements,
-                                      max_degree=self.basis_max_degree,
-                                      cutoff_fn=partial(e3x.nn.smooth_cutoff, cutoff=self.cutoff),
-                                      )
-    for moment in range(self.max_moment - 1):
-      y = e3x.nn.FusedTensor(max_degree=self.moment_max_degree)(y)
+                     max_degree=self.basis_max_degree,
+                     num=self.num_basis_features,
+                     radial_fn=self.radial_function,
+                     cutoff_fn=partial(e3x.nn.smooth_cutoff, cutoff=self.cutoff)
+                    )
+    # The line above is our 'first moment' since we have directional information.
+    # Each subsequent moment happens down here.
+    for _ in range(self.max_moment - 1):
+      y = self.tensor_module(features=self.num_moment_features, max_degree=self.moment_max_degree)(y)
     
     return y
