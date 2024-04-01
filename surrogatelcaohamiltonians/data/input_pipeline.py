46c9b2666664130f7bdd5aadab8cb4fe2dec9329
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-01 09:30:17 -0700

    Initial input pipeline work

---
+++ surrogatelcaohamiltonians/data/input_pipeline.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Dict, Iterator
+from pathlib import Path
+import json
+
+from ase.io import read
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import tensorflow as tf
+
+log = logging.getLogger(__name__)
+
+def pairwise_hamiltonian_from_file(filename: Path):
+  data = np.load(filename)
+  keys = data.keys()
+  bond_atom_indices = np.column_stack([key[0:2] for key in keys])
+  bond_vectors = np.column_stack([[key[2:]] for key in keys])
+  hblocks = [block for block in data.values()]
+  return bond_atom_indices, bond_vectors, hblocks
+
+# TODO Need not be a json specificallyh, we'll see
+def orbital_spec_from_file(filename: Path):
+  return json.load(open(filename, mode='r'))
+
+
+def snapshot_from_directory(
+    directory: Path,
+    atoms_filename: str = "atoms.extxyz",
+    orbital_spec_filename: str = "orbital_ells.json",
+    hamiltonian_dataset_filename: str = "hamiltonians.npz"
+):
+  atoms = read(directory / atoms_filename)
+  orbital_spec = orbital_spec_from_file(directory / orbital_spec_filename)
+  bond_atom_indices, bond_vectors, hamiltonian_blocks = pairwise_hamiltonian_from_file(directory / hamiltonian_dataset_filename)
