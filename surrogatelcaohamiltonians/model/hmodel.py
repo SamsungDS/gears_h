c7e1ed4f77a27aaaf7d5a22a4dbe20f7149721b2
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-05 09:32:53 -0700

    Test model that compiles

--- surrogatelcaohamiltonians/model/hmodel.py
+++ surrogatelcaohamiltonians/model/hmodel.py
@@ -9,13 +9,13 @@ from surrogatelcaohamiltonians.layers import (
 )
 
 
-class HamiltonianModel(nn.module):
-    atom_centered = AtomCenteredTensorMomentDescriptor(
-        SpeciesAwareRadialBasis(cutoff=8.0)
+class HamiltonianModel(nn.Module):
+    atom_centered: AtomCenteredTensorMomentDescriptor = AtomCenteredTensorMomentDescriptor(
+        SpeciesAwareRadialBasis(cutoff=8.0),
     )
-    bond_centered = BondCenteredTensorMomentDescriptor(cutoff=8.0)
-    dense = DenseBlock()
-    readout = Readout(2, max_ell=2)
+    bond_centered: BondCenteredTensorMomentDescriptor = BondCenteredTensorMomentDescriptor(cutoff=8.0)
+    dense: DenseBlock = DenseBlock()
+    readout: Readout = Readout(2, max_ell=2)
 
     @nn.compact
     def __call__(self, atomic_numbers, neighbour_indices, neighbour_displacements):
