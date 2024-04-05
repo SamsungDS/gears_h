8d7728d62b7f1068313915a18e40f8b53b303b38
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-05 09:33:21 -0700

    Skeleton trainer enough to go through 1 epoch

--- surrogatelcaohamiltonians/train/trainer.py
+++ surrogatelcaohamiltonians/train/trainer.py
@@ -1,3 +1,5 @@
+from time import time
+
 import jax.numpy as jnp
 import jax
 import optax
@@ -5,11 +7,16 @@ import optax
 from surrogatelcaohamiltonians.data.input_pipeline import InMemoryDataset
 
 
-def train_step(params, model_apply, optimizer_update, batch):
+def train_step(params, model_apply, optimizer_update, batch_full, opt_state):
+  batch, batch_labels = batch_full
   def loss_function(params):
-    h_irreps_predicted = model_apply(atomic_numbers=batch["numbers"], neighbour_indices=batch["idx_ij"], neighbour_displacements=batch=["idx_D"])
+    h_irreps_predicted = model_apply(
+      params,
+      atomic_numbers=batch["numbers"], 
+      neighbour_indices=batch["idx_ij"], 
+      neighbour_displacements=batch["idx_D"])
 
-    loss = jnp.mean( ((h_irreps_predicted - batch["h_irreps"]) * batch["mask"]) ** 2)
+    loss = jnp.mean( ((h_irreps_predicted - batch_labels["h_irreps"]) * batch_labels["mask"]) ** 2)
     return loss, h_irreps_predicted
   
   (loss, h_irreps_predicted), grad = jax.value_and_grad(loss_function, has_aux=True)(params)
@@ -22,11 +29,23 @@ def train_step(params, model_apply, optimizer_update, batch):
 
 def fit(model, 
         train_dataset: InMemoryDataset,
-        loss_function,
+        # loss_function,
         n_epochs,
         ):
   # TODO: The val step
+  steps_per_epoch = 5 # train_dataset.steps_per_epoch()
+  train_dataset = iter(train_dataset)
 
-  steps_per_epoch = train_dataset.steps_per_epoch()
-  for epoch in range(1, n_epochs + 1):
-    for ibatch in r
+  _batch_inputs, _batch_labels = next(train_dataset)
+  params = model.init(jax.random.PRNGKey(2462), _batch_inputs["numbers"], _batch_inputs["idx_ij"], _batch_inputs["idx_D"])
+  optimizer = optax.adam(learning_rate=1e-3)
+  opt_state = optimizer.init(params)
+
+
+  for iepoch in range(1, n_epochs + 1):
+    print("Epoch:", iepoch)
+    for ibatch in range(steps_per_epoch): 
+      print("Batch:", ibatch)
+      tstart = time()
+      params, opt_state, loss = train_step(params, model.apply, optimizer.update, next(train_dataset), opt_state)
+      print("Batch:", ibatch, "in", time() - tstart, "seconds. Loss:", loss)
