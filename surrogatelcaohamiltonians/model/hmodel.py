90f4db0e5a9f2ce18ee1c47303dc7be2b50a0eb8
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-05 08:23:16 -0700

    First draft of hamiltonian model, no onsite yet

---
+++ surrogatelcaohamiltonians/model/hmodel.py
@@ -0,0 +1,19 @@
+import flax.linen as nn
+
+from surrogatelcaohamiltonians.layers import AtomCenteredTensorMomentDescriptor, BondCenteredTensorMomentDescriptor, SpeciesAwareRadialBasis, DenseBlock, Readout
+
+
+
+class HamiltonianModel(nn.module):
+  atom_centered = AtomCenteredTensorMomentDescriptor(SpeciesAwareRadialBasis(cutoff=8.0))
+  bond_centered = BondCenteredTensorMomentDescriptor(cutoff=8.0)
+  dense = DenseBlock()
+  readout = Readout(2, max_ell=2)
+
+  @nn.compact
+  def __call__(self, atomic_numbers, neighbour_indices, neighbour_displacements):
+    y = self.atom_centered(atomic_numbers, neighbour_indices, neighbour_displacements)
+    y = self.bond_centered(y, neighbour_indices, neighbour_displacements)
+    y = self.dense(y)
+    h = self.readout(y)
+    return h
