7113bedf6e9d3e17ff827c417ec86f29f60720e2
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-16 06:50:39 -0700

    Scaffolding aond continuously changing training loop

--- slh/train/trainer.py
+++ slh/train/trainer.py
@@ -1,5 +1,8 @@
 from time import time
 from functools import partial
+import logging
+
+log = logging.getLogger(__name__)
 
 import flax.linen as nn
 import jax.numpy as jnp
@@ -24,18 +27,29 @@ def train_step(params, model_apply, optimizer_update, batch_full, opt_state):
             batch["idx_D"],
         )
 
+        # loss = jnp.mean(
+        #     (h_irreps_predicted - batch_labels["h_irreps"]) ** 2,
+        #     where=batch_labels["mask"],
+        # )
+
+        assert h_irreps_predicted.shape == batch_labels["h_irreps"].shape == batch_labels["mask"].shape, "This happens when your readout and your labels are not consistent."
+
         loss = jnp.mean(
-            (h_irreps_predicted - batch_labels["h_irreps"]) ** 2,
+            optax.huber_loss(h_irreps_predicted, batch_labels["h_irreps"]),
             where=batch_labels["mask"],
         )
-        return loss, h_irreps_predicted
 
-    (loss, h_irreps_predicted), grad = jax.value_and_grad(loss_function, has_aux=True)(
-        params
-    )
+        return loss, jnp.mean(
+            jnp.abs(h_irreps_predicted - batch_labels["h_irreps"]),
+            where=batch_labels["mask"],
+        )
+
+    (loss, mae_loss), grad = jax.value_and_grad(loss_function, has_aux=True)(params)
+    # grad = optax.adaptive_grad_clip(0.1)
     updates, opt_state = optimizer_update(grad, opt_state, params)
+    # updates, opt_state = optax.chain()
     params = optax.apply_updates(params, updates)
-    return params, opt_state, loss
+    return params, opt_state, grad, mae_loss
 
 
 def fit(
@@ -75,22 +89,37 @@ def fit(
     # #     _batch_inputs["idx_ij"],
     # #     _batch_inputs["idx_D"],
     # # )
-    optimizer = optax.adam(learning_rate=1e-4)
+    # optimizer = optax.adamaxw(learning_rate=optax.warmup_cosine_decay_schedule(1e-4, 5e-3, 5, 20, 1e-4))# , nesterov=True)
+    optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(params)
+    mae_loss = jnp.nan
+    epoch_pbar = trange(n_epochs, desc="Epochs", ncols=75, disable=False)
 
-    for iepoch in range(n_epochs):
-        print("Epoch:", iepoch)
-        for ibatch in trange(steps_per_epoch):
-            tstart = time()
-            params, opt_state, loss = train_step(
-                params,
-                model_apply,
-                optimizer.update,
-                next(batch_train_dataset),
-                opt_state,
-            )
-            # jax.block_until_ready(loss)
-            # print("Batch:", ibatch, "in", time() - tstart, "seconds. Loss:", loss)
-        print("Loss:", loss)
+    params_list, grad_list = [], []
+    try:
+        for _ in epoch_pbar:
+            batch_pbar = trange(steps_per_epoch, desc="Batch", leave=False, ncols=75, smoothing=0.0, disable=False)
+            epoch_mae_loss = 0.0
+            for _ in batch_pbar:
+                batch_data = next(batch_train_dataset)
+                # log.info(f"{batch_data[1]['h_irreps'].shape}")
+                params, opt_state, grad, mae_loss = train_step(
+                    params,
+                    model_apply,
+                    optimizer.update,
+                    batch_data,
+                    opt_state,
+                )
+                epoch_mae_loss += mae_loss
+                # params_list.append(params)
+                # grad_list.append(grad)
 
-    # train_dataset.cleanup()
+                batch_pbar.set_postfix(mae=f"{mae_loss / train_dataset.batch_size:0.3e}")
+                batch_pbar.update()
+            
+            epoch_pbar.set_postfix(mae=f"{epoch_mae_loss / (steps_per_epoch  * train_dataset.batch_size):0.3e}")
+            epoch_pbar.update()
+    except StopIteration:
+        print("Yes the stopiteration")
+    
+    return params_list, grad_list
