4e7cf7d88db8d2f7d533b2b11b7a4aebd7b8ddd3
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-04-30 12:41:06 -0700

    More flexible bond-centered descriptor

--- slh/layers/descriptor/bondcentered.py
+++ slh/layers/descriptor/bondcentered.py
@@ -15,6 +15,8 @@ class BondCenteredTensorMomentDescriptor(nn.Module):
     # num_basis_features: int = 8
     # basis_max_degree: int = 3
     # num_moment_features: int = 64 # TODO this can in principle be a list of ints
+    max_actp_degree: int = 4
+    max_basis_degree: int = 2
     max_degree: int = 4
     tensor_module: Union[e3x.nn.Tensor, e3x.nn.FusedTensor] = e3x.nn.FusedTensor
     # radial_function = e3x.nn.basic_fourier
@@ -29,14 +31,15 @@ class BondCenteredTensorMomentDescriptor(nn.Module):
             atomic_descriptors[neighbours_j],
         )
         y = self.tensor_module(
-            max_degree=self.max_degree, name="atompair_td", cartesian_order=False
+            max_degree=self.max_actp_degree,
+            name="atompair_tp", cartesian_order=False
         )(atom1_desc, atom2_desc)
 
         # We put in information about the orientation/length of the bond vector here
         bond_expansion = e3x.nn.basis(
             neighbour_displacements,
             num=num_radial_features,
-            max_degree=self.max_degree,
+            max_degree=self.max_basis_degree,
             radial_fn=partial(jinclike, limit=self.cutoff),
             cutoff_fn=partial(e3x.nn.smooth_cutoff, cutoff=self.cutoff),
             damping_fn=partial(e3x.nn.smooth_damping, gamma=5.0),
@@ -45,7 +48,9 @@ class BondCenteredTensorMomentDescriptor(nn.Module):
 
         # num_pairs x 2 x (max_degree + 1)^2 x num_radial_features
         # y = e3x.nn.add(y, bond_expansion)
-        y = self.tensor_module(max_degree=self.max_degree, cartesian_order=False)(
+        y = self.tensor_module(max_degree=self.max_degree, cartesian_order=False, 
+                               # kernel_init=partial(e3x.nn.initializers.fused_tensor_normal, scale=1e-2)
+                               )(
             y, bond_expansion
         )
         # y = jnp.concat([y, e3x.nn.change_max_degree_or_type(bond_expansion, include_pseudotensors=True)], axis=-1)
