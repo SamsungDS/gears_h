8510c5aa7e70e5e673e0eeb45566fa01518b78a3
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-06-18 01:10:48 +0000

    Blacken some more

--- slh/layers/descriptor/atomcentered.py
+++ slh/layers/descriptor/atomcentered.py
@@ -6,9 +6,7 @@ import flax.linen as nn
 
 from jaxtyping import Float, Array, Int
 
-from slh.layers.descriptor.radial_basis import (
-    SpeciesAwareRadialBasis, jinclike
-)
+from slh.layers.descriptor.radial_basis import SpeciesAwareRadialBasis, jinclike
 
 from dataclasses import field
 
@@ -105,6 +103,7 @@ class AtomCenteredTensorMomentDescriptor(nn.Module):
 
         return y + e3x.nn.mish(y)
 
+
 class MPAtomCenteredDescriptor(nn.Module):
     radial_basis: SpeciesAwareRadialBasis
     use_fused_tensor: bool = False
@@ -133,7 +132,9 @@ class MPAtomCenteredDescriptor(nn.Module):
 
         num_neighbour_normalization = self.param(
             "neighbour_normalization",
-            nn.initializers.constant((len(neighbour_indices) / len(atomic_numbers)) ** 0.5),
+            nn.initializers.constant(
+                (len(neighbour_indices) / len(atomic_numbers)) ** 0.5
+            ),
             1,
             dtype=jnp.float32,
         )
@@ -144,22 +145,28 @@ class MPAtomCenteredDescriptor(nn.Module):
         ).astype(jnp.float32)
 
         for _ in range(2):
-            y = e3x.nn.MessagePass(
-                max_degree=3,
-                use_basis_bias=True,
-                cartesian_order=False,
-                use_fused_tensor=self.use_fused_tensor
-            )(y, e3x.nn.basis(
-                neighbour_displacements,
-                max_degree=2,
-                num=16,
-                radial_fn=partial(e3x.nn.sinc, limit=self.radial_basis.cutoff),
-                cartesian_order=False),
-                src_idx=neighbour_indices[:, 1],
-                dst_idx=neighbour_indices[:, 0],
-                num_segments=len(atomic_numbers)
-                ) / num_neighbour_normalization
-            
+            y = (
+                e3x.nn.MessagePass(
+                    max_degree=3,
+                    use_basis_bias=True,
+                    cartesian_order=False,
+                    use_fused_tensor=self.use_fused_tensor,
+                )(
+                    y,
+                    e3x.nn.basis(
+                        neighbour_displacements,
+                        max_degree=2,
+                        num=16,
+                        radial_fn=partial(e3x.nn.sinc, limit=self.radial_basis.cutoff),
+                        cartesian_order=False,
+                    ),
+                    src_idx=neighbour_indices[:, 1],
+                    dst_idx=neighbour_indices[:, 0],
+                    num_segments=len(atomic_numbers),
+                )
+                / num_neighbour_normalization
+            )
+
             y = e3x.nn.Dense(features=y.shape[-1])(y) + y
 
         if self.embedding_residual_connection:
@@ -167,7 +174,7 @@ class MPAtomCenteredDescriptor(nn.Module):
                 y, self.embedding_transformation(self.embedding(atomic_numbers))
             )
 
-        return y # + e3x.nn.mish(y)
+        return y  # + e3x.nn.mish(y)
 
 
 class SAAtomCenteredDescriptor(nn.Module):
@@ -187,11 +194,11 @@ class SAAtomCenteredDescriptor(nn.Module):
             param_dtype=jnp.float32,
         )
 
-        self.mp_block = e3x.nn.SelfAttention (
-                max_degree=self.mp_degree,
-                cartesian_order=False,
-                **self.mp_options,
-            )
+        self.mp_block = e3x.nn.SelfAttention(
+            max_degree=self.mp_degree,
+            cartesian_order=False,
+            **self.mp_options,
+        )
 
     @nn.compact
     def __call__(
@@ -212,27 +219,28 @@ class SAAtomCenteredDescriptor(nn.Module):
         ).astype(jnp.float32)
 
         # num_atoms x 1 x L x F
-        y = (
-            e3x.ops.indexed_sum(y, dst_idx=idx_i, num_segments=len(atomic_numbers))
-        )
+        y = e3x.ops.indexed_sum(y, dst_idx=idx_i, num_segments=len(atomic_numbers))
 
         for _ in range(self.mp_steps):
-            y = e3x.nn.SelfAttention (
+            y = e3x.nn.SelfAttention(
                 max_degree=self.mp_degree,
                 use_basis_bias=True,
                 cartesian_order=False,
                 use_fused_tensor=self.use_fused_tensor,
                 num_heads=4,
-            )(y, e3x.nn.basis(
-                neighbour_displacements,
-                max_degree=2,
-                num=16,
-                radial_fn=partial(e3x.nn.sinc, limit=self.radial_basis.cutoff),
-                cartesian_order=False),
+            )(
+                y,
+                e3x.nn.basis(
+                    neighbour_displacements,
+                    max_degree=2,
+                    num=16,
+                    radial_fn=partial(e3x.nn.sinc, limit=self.radial_basis.cutoff),
+                    cartesian_order=False,
+                ),
                 src_idx=idx_j,
                 dst_idx=idx_i,
-                num_segments=len(atomic_numbers)
-                )
+                num_segments=len(atomic_numbers),
+            )
 
         if self.embedding_residual_connection:
             y = e3x.nn.add(
