18f667d5268858c265a62c6d4a7755317c2a775b
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-06-18 01:11:32 +0000

    Add initial functions to checkpointing

--- slh/train/checkpoints.py
+++ slh/train/checkpoints.py
@@ -1,4 +1,4 @@
-# Large parts of this file are from https://github.com/apax-hub/apax/blob/dev/apax/train/checkpoints.py
+# Taken almost entirely from https://github.com/apax-hub/apax/blob/dev/apax/train/checkpoints.py
 import logging
 
 import jax
@@ -12,7 +12,8 @@ from pathlib import Path
 log = logging.getLogger(__name__)
 
 
-def check_for_ensemble(params: FrozenDict) -> int:    """Checks if a set of parameters belongs to an ensemble model.
+def check_for_ensemble(params: FrozenDict) -> int:
+    """Checks if a set of parameters belongs to an ensemble model.
     This is the case if all parameters share the same first dimension (parameter batch)
     """
     flat_params = flatten_dict(params)
@@ -24,6 +25,7 @@ def check_for_ensemble(params: FrozenDict) -> int:    """Checks if a set of para
     else:
         return 1
 
+
 def create_train_state(model, params: FrozenDict, tx):
     n_models = check_for_ensemble(params)
 
@@ -31,7 +33,7 @@ def create_train_state(model, params: FrozenDict, tx):
         state = train_state.TrainState.create(
             apply_fn=model,
             params=params,
-            tx=tx,
+            tx=tx,  # This is an optimizer state
         )
         return state
 
@@ -41,6 +43,7 @@ def create_train_state(model, params: FrozenDict, tx):
         train_state_fn = create_single_train_state
 
     return train_state_fn(params)
+
 
 def create_params(model, rng_key, sample_input: tuple, n_models: int):
     keys = jax.random.split(rng_key, num=n_models + 1)
@@ -62,6 +65,25 @@ def create_params(model, rng_key, sample_input: tuple, n_models: int):
 
     return params, rng_key
 
+
+def load_state(state, ckpt_dir):
+    start_epoch = 0
+    target = {"model": state, "epoch": 0}
+    checkpoints_exist = Path(ckpt_dir).is_dir()
+    if checkpoints_exist:
+        log.info("Loading checkpoint")
+        raw_restored = checkpoints.restore_checkpoint(
+            ckpt_dir, target=target, step=None
+        )
+        state = raw_restored["model"]
+        start_epoch = raw_restored["epoch"] + 1
+        log.info(
+            "Successfully restored checkpoint from epoch %d", raw_restored["epoch"]
+        )
+
+    return state, start_epoch
+
+
 class CheckpointManager:
     def __init__(self) -> None:
         self.async_manager = checkpoints.AsyncManager()
@@ -75,3 +97,56 @@ class CheckpointManager:
             keep=2,
             async_manager=self.async_manager,
         )
+
+def stack_parameters(param_list: List[FrozenDict]) -> FrozenDict:
+    """Combine a list of parameter sets into a stacked version.
+    Used for model ensembles.
+    """
+    flat_param_list = []
+    for params in param_list:
+        params = unfreeze(params)
+        flat_params = flatten_dict(params)
+        flat_param_list.append(flat_params)
+
+    stacked_flat_params = flat_params
+    for p in flat_param_list[0].keys():
+        stacked_flat_params[p] = jnp.stack(
+            [flat_param[p] for flat_param in flat_param_list]
+        )
+
+    stacked_params = unflatten_dict(stacked_flat_params)
+    stack_params = freeze(stacked_params)
+    return stack_params
+
+
+def load_params(model_version_path: Path, best=True) -> FrozenDict:
+    model_version_path = Path(model_version_path)
+    if best:
+        model_version_path = model_version_path / "best"
+    log.info(f"loading checkpoint from {model_version_path}")
+    try:
+        # keep try except block for zntrack load from rev
+        raw_restored = checkpoints.restore_checkpoint(
+            model_version_path, target=None, step=None
+        )
+    except FileNotFoundError:
+        print(f"No checkpoint found at {model_version_path}")
+    if raw_restored is None:
+        raise FileNotFoundError(f"No checkpoint found at {model_version_path}")
+    params = jax.tree_map(jnp.asarray, raw_restored["model"]["params"])
+
+    return params
+
+
+def restore_single_parameters(model_dir: Path) -> Tuple[Config, FrozenDict]:
+    """Load the config and parameters of a single model"""
+    model_dir = Path(model_dir)
+    model_config = parse_config(model_dir / "config.yaml")
+
+    if model_config.data.experiment == "":
+        model_config.data.directory = model_dir.resolve().as_posix()
+    else:
+        model_config.data.directory = model_dir.parent.resolve().as_posix()
+
+    ckpt_dir = model_config.data.model_version_path
+    return model_config, load_params(ckpt_dir)
