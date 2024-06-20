63f3002ebd797d9f9fc214a6cabd18919476c95f
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-06-20 13:38:48 +0000

    Dummy builder draft works

--- slh/model/builder.py
+++ slh/model/builder.py
@@ -1,10 +1,15 @@
+from functools import partial
 from slh.layers.descriptor import (AtomCenteredTensorMomentDescriptor,
                                    SAAtomCenteredDescriptor,
                                    BondCenteredTensorMomentDescriptor,
                                    SpeciesAwareRadialBasis)
 from slh.layers.readout import Readout
 from slh.layers.residual_dense import DenseBlock
+from slh.model.hmodel import HamiltonianModel
 
+import jax.numpy as jnp
+import jax
+import e3x
 
 def build_model(config, readout_config):
     radial_descriptor = SpeciesAwareRadialBasis(
@@ -27,121 +32,155 @@ def build_model(config, readout_config):
     )
 
 
-class ModelBuilder:
-    def __init__(self, model_config: ModelConfig, n_species: int = 119):
-        self.config = model_config
-        self.n_species = n_species
+# class ModelBuilder:
+#     def __init__(self, model_config: ModelConfig, n_species: int = 119):
+#         self.config = model_config
+#         self.n_species = n_species
 
-    def build_basis_function(self):
-        basis_fn = GaussianBasis(
-            n_basis=self.config["n_basis"],
-            r_min=self.config["r_min"],
-            r_max=self.config["r_max"],
-            dtype=self.config["descriptor_dtype"],
-        )
-        return basis_fn
+#     def build_basis_function(self):
+#         basis_fn = GaussianBasis(
+#             n_basis=self.config["n_basis"],
+#             r_min=self.config["r_min"],
+#             r_max=self.config["r_max"],
+#             dtype=self.config["descriptor_dtype"],
+#         )
+#         return basis_fn
 
-    def build_radial_function(self):
-        basis_fn = self.build_basis_function()
-        radial_fn = RadialFunction(
-            n_radial=self.config["n_radial"],
-            basis_fn=basis_fn,
-            n_species=self.n_species,
-            emb_init=self.config["emb_init"],
-            dtype=self.config["descriptor_dtype"],
-        )
-        return radial_fn
+#     def build_radial_function(self):
+#         basis_fn = self.build_basis_function()
+#         radial_fn = RadialFunction(
+#             n_radial=self.config["n_radial"],
+#             basis_fn=basis_fn,
+#             n_species=self.n_species,
+#             emb_init=self.config["emb_init"],
+#             dtype=self.config["descriptor_dtype"],
+#         )
+#         return radial_fn
 
-    def build_descriptor(
-        self,
-        apply_mask,
-    ):
-        radial_fn = self.build_radial_function()
-        descriptor = GaussianMomentDescriptor(
-            radial_fn=radial_fn,
-            n_contr=self.config["n_contr"],
-            dtype=self.config["descriptor_dtype"],
-            apply_mask=apply_mask,
-        )
-        return descriptor
+#     def build_descriptor(
+#         self,
+#         apply_mask,
+#     ):
+#         radial_fn = self.build_radial_function()
+#         descriptor = GaussianMomentDescriptor(
+#             radial_fn=radial_fn,
+#             n_contr=self.config["n_contr"],
+#             dtype=self.config["descriptor_dtype"],
+#             apply_mask=apply_mask,
+#         )
+#         return descriptor
 
-    def build_readout(self):
-        readout = AtomisticReadout(
-            units=self.config["nn"],
-            b_init=self.config["b_init"],
-            dtype=self.config["readout_dtype"],
-        )
-        return readout
+#     def build_readout(self):
+#         readout = AtomisticReadout(
+#             units=self.config["nn"],
+#             b_init=self.config["b_init"],
+#             dtype=self.config["readout_dtype"],
+#         )
+#         return readout
 
-    def build_scale_shift(self, scale, shift):
-        scale_shift = PerElementScaleShift(
-            n_species=self.n_species,
-            scale=scale,
-            shift=shift,
-            dtype=self.config["scale_shift_dtype"],
-        )
-        return scale_shift
+#     def build_scale_shift(self, scale, shift):
+#         scale_shift = PerElementScaleShift(
+#             n_species=self.n_species,
+#             scale=scale,
+#             shift=shift,
+#             dtype=self.config["scale_shift_dtype"],
+#         )
+#         return scale_shift
 
-    def build_atomistic_model(
-        self,
-        scale,
-        shift,
-        apply_mask,
-    ):
-        descriptor = self.build_descriptor(apply_mask)
-        readout = self.build_readout()
-        scale_shift = self.build_scale_shift(scale, shift)
+#     def build_atomistic_model(
+#         self,
+#         scale,
+#         shift,
+#         apply_mask,
+#     ):
+#         descriptor = self.build_descriptor(apply_mask)
+#         readout = self.build_readout()
+#         scale_shift = self.build_scale_shift(scale, shift)
 
-        atomistic_model = AtomisticModel(descriptor, readout, scale_shift)
-        return atomistic_model
+#         atomistic_model = AtomisticModel(descriptor, readout, scale_shift)
+#         return atomistic_model
 
-    def build_energy_model(
-        self,
-        scale=1.0,
-        shift=0.0,
-        apply_mask=True,
-        init_box: np.array = np.array([0.0, 0.0, 0.0]),
-        inference_disp_fn=None,
-    ):
-        atomistic_model = self.build_atomistic_model(
-            scale,
-            shift,
-            apply_mask,
-        )
-        corrections = []
-        if self.config["use_zbl"]:
-            repulsion = ZBLRepulsion(
-                apply_mask=apply_mask,
-                r_max=self.config["r_max"],
+#     def build_energy_model(
+#         self,
+#         scale=1.0,
+#         shift=0.0,
+#         apply_mask=True,
+#         init_box: np.array = np.array([0.0, 0.0, 0.0]),
+#         inference_disp_fn=None,
+#     ):
+#         atomistic_model = self.build_atomistic_model(
+#             scale,
+#             shift,
+#             apply_mask,
+#         )
+#         corrections = []
+#         if self.config["use_zbl"]:
+#             repulsion = ZBLRepulsion(
+#                 apply_mask=apply_mask,
+#                 r_max=self.config["r_max"],
+#             )
+#             corrections.append(repulsion)
+
+#         model = EnergyModel(
+#             atomistic_model,
+#             corrections=corrections,
+#             init_box=init_box,
+#             inference_disp_fn=inference_disp_fn,
+#         )
+#         return model
+
+#     def build_energy_derivative_model(
+#         self,
+#         scale=1.0,
+#         shift=0.0,
+#         apply_mask=True,
+#         init_box: np.array = np.array([0.0, 0.0, 0.0]),
+#         inference_disp_fn=None,
+#     ):
+#         energy_model = self.build_energy_model(
+#             scale,
+#             shift,
+#             apply_mask,
+#             init_box=init_box,
+#             inference_disp_fn=inference_disp_fn,
+#         )
+
+#         model = EnergyDerivativeModel(
+#             energy_model,
+#             calc_stress=self.config["calc_stress"],
+#         )
+#         return model
+
+
+def build_lcao_hamiltonian_model(dummy):
+    model = HamiltonianModel(
+        atom_centered=(
+            SAAtomCenteredDescriptor(
+                SpeciesAwareRadialBasis(
+                    cutoff=6.8,
+                    max_degree=2,
+                    num_elemental_embedding=8,
+                    num_radial=16,
+                    tensor_module=partial(e3x.nn.Tensor, param_dtype=jnp.bfloat16),
+                    embedding_residual_connection=True,
+                ),
+                use_fused_tensor=False,
+                embedding_residual_connection=True,
             )
-            corrections.append(repulsion)
-
-        model = EnergyModel(
-            atomistic_model,
-            corrections=corrections,
-            init_box=init_box,
-            inference_disp_fn=inference_disp_fn,
-        )
-        return model
-
-    def build_energy_derivative_model(
-        self,
-        scale=1.0,
-        shift=0.0,
-        apply_mask=True,
-        init_box: np.array = np.array([0.0, 0.0, 0.0]),
-        inference_disp_fn=None,
-    ):
-        energy_model = self.build_energy_model(
-            scale,
-            shift,
-            apply_mask,
-            init_box=init_box,
-            inference_disp_fn=inference_disp_fn,
-        )
-
-        model = EnergyDerivativeModel(
-            energy_model,
-            calc_stress=self.config["calc_stress"],
-        )
-        return model
+        ),
+        bond_centered=(
+            BondCenteredTensorMomentDescriptor(
+                cutoff=6.8,
+                max_actp_degree=4,
+                max_basis_degree=4,
+                max_degree=4,
+                tensor_module=partial(e3x.nn.Tensor, param_dtype=jnp.bfloat16),
+            )
+        ),
+        dense=DenseBlock(
+            dense_layer=partial(e3x.nn.Dense, param_dtype=jnp.bfloat16),
+            layer_widths=[128],
+        ),
+        readout=Readout(2, max_ell=2),
+    )
+    return model
