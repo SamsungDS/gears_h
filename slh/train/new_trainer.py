0257299fce6aa08dc2addc5f8e52e8a66a2d8dd5
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-06-28 22:22:25 +0000

    Pull Huber loss out of loss calculator, and corresponding changes to make_step_function.

--- slh/train/new_trainer.py
+++ slh/train/new_trainer.py
@@ -33,7 +33,17 @@ def fit(state: TrainState,
     
     return
 
-def calculate_loss(params):
+def huber_loss(h_irreps_predicted, batch_labels):
+    loss = jnp.mean(optax.huber_loss(h_irreps_predicted, batch_labels["h_irreps"]),
+                    where=batch_labels["mask"])
+
+    mae_loss = jnp.mean(jnp.abs(h_irreps_predicted - batch_labels["h_irreps"]),
+                        where=batch_labels["mask"])
+    return loss, mae_loss
+
+def calculate_loss(params, batch_full, loss_function, model):
+    batch, batch_labels = batch_full
+    model_apply = jax.vmap(model.apply, in_axes=(None, 0, 0, 0))
     # TODO Make loss function an argument and allow user input.
     h_irreps_predicted = model_apply(
         params,
@@ -42,39 +52,45 @@ def calculate_loss(params):
         batch["idx_D"],
     )
 
+    # TODO Remove this when we make the readout layer size automatically calculated.
     assert (
         h_irreps_predicted.shape
         == batch_labels["h_irreps"].shape
         == batch_labels["mask"].shape
     ), "This happens when your readout and your labels are not consistent."
 
-    loss = jnp.mean(
-        optax.huber_loss(h_irreps_predicted, batch_labels["h_irreps"]),
-        where=batch_labels["mask"]
-    )
-
-    mae_loss = jnp.mean(jnp.abs(h_irreps_predicted - batch_labels["h_irreps"]),
-                        where=batch_labels["mask"])
+    loss, mae_loss = loss_function(h_irreps_predicted, batch_labels)
 
     return loss, mae_loss
 
-def make_step_functions(loss_function, logging_metrics, model):
+def make_step_functions(loss_function = huber_loss, logging_metrics, model):
+    loss_calculator = partial(calculate_loss, loss_function=loss_function, model=model)
+    grad_fn = jax.value_and_grad(loss_calculator, 0, has_aux=True)
 
-    grad_fn = jax.value_and_grad(loss_function, 0, has_aux=True)
-
-    def update_step(state, inputs, labels):
-        (loss, mae_loss), grads = grad_fn(state.params)
+    def update_step(state, batch_full):
+        (loss, mae_loss), grads = grad_fn(state.params, batch_full)
         state = state.apply_gradients(grads=grads)
         return loss, mae_loss, state
+    
+    # TODO add support for ensemble models.
 
     @partial(jax.jit, static_argnames=("model_apply", "optimizer"))
-    def train_step(params: optax.Params,
-                   model_apply: callable,
-                   optimizer,
-                   batch_full_list: list,
-                   opt_state: OptimizerState,):
+    def train_step(state, batch):
+
+        loss, mae_loss, state = update_step()
         
         return params, opt_state, grad, loss, step_mae_loss
+    
+    # TODO OLD KEPT JUST IN CASE. Remove when no longer needed.
+    # @partial(jax.jit, static_argnames=("model_apply", "optimizer"))
+    # def train_step(params: optax.Params,
+    #                model_apply: callable,
+    #                optimizer,
+    #                batch_full_list: list,
+    #                opt_state: OptimizerState,):
+        
+    #     return params, opt_state, grad, loss, step_mae_loss
+    
     
     @partial(jax.jit, static_argnames=("model_apply",))
     def val_step(params, 
