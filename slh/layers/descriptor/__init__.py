7a155f0f38e92c8de0630e4bc2c8bb5d24c26e5c
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-07-12 19:23:12 +0000

    Remove depracated descriptors from init

--- slh/layers/descriptor/__init__.py
+++ slh/layers/descriptor/__init__.py
@@ -1,4 +1,4 @@
 from .radial_basis import SpeciesAwareRadialBasis
-from .atomcentered import AtomCenteredTensorMomentDescriptor, MPAtomCenteredDescriptor, SAAtomCenteredDescriptor, TDSAAtomCenteredDescriptor
+from .atomcentered import SAAtomCenteredDescriptor, TDSAAtomCenteredDescriptor
 from .bondcentered import BondCenteredTensorMomentDescriptor
 from .radial_basis import SpeciesAwareRadialBasis
