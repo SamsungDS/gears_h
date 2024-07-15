Fix hard coded data type in hmodel and remove calculation of exponential scale correction (for now).

slh/model/hmodel.py
@@ -25,8 +25,8 @@ class HamiltonianModel(nn.Module):
             atomic_numbers, neighbour_indices, neighbour_displacements
         )
 
-        atom_centered_descriptors = atom_centered_descriptors.astype(jnp.float32)
-        assert atom_centered_descriptors.dtype == jnp.float32
+        # atom_centered_descriptors = atom_centered_descriptors.astype(jnp.float32)
+        # assert atom_centered_descriptors.dtype == jnp.float32
 
         # atom_centered_descriptors = e3x.nn.MessagePass(max_degree=2, cartesian_order=False)(
         #     inputs=atom_centered_descriptors,
@@ -54,7 +54,7 @@ class HamiltonianModel(nn.Module):
 
         off_diagonal_denseout = self.dense(bc_features)
         off_diagonal_irreps = self.readout(off_diagonal_denseout)
-        scaling_correction = ExponentialScaleCorrection(
-            self.readout.nfeatures, self.readout.max_ell
-        )(jnp.linalg.norm(neighbour_displacements, axis=-1, keepdims=True))
+        # scaling_correction = ExponentialScaleCorrection(
+        #     self.readout.nfeatures, self.readout.max_ell
+        # )(jnp.linalg.norm(neighbour_displacements, axis=-1, keepdims=True))
         return off_diagonal_irreps  #  * scaling_correction