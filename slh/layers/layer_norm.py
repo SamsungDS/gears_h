13ddde23d869c3e42c9100ae0f47d89a049508dd
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-08-02 21:47:29 +0000

    Layer norm skeleton

---
+++ slh/layers/layer_norm.py
@@ -0,0 +1,12 @@
+import e3x
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+from jaxtyping import Array, Float, Int
+
+class LayerNorm(nn.module):
+
+    def setup(self):
+        
+
+    def __call__(self):
