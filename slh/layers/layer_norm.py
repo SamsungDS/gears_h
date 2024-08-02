f0502237fa2073ff2e1101e4cdb2ee1b3e8efb55
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-08-02 21:56:37 +0000

    Expand parameter array to the correct dimensions.

--- slh/layers/layer_norm.py
+++ slh/layers/layer_norm.py
@@ -9,11 +9,13 @@ class LayerNorm(nn.module):
     max_ell: Int
 
     def setup(self):
+        idx_l = e3x.nn.modules._duplication_indices_for_max_degree(self.max_ell)
         # 1 x P x L x F
         self.irrepwise_scaling = self.param("irrepwise_scaling",
                                             nn.initializers.constant(1.0),
                                             shape = (1, 1, self.max_ell+1, self.nfeatures)
                                            )
+        self.irrepwise_scaling = jnp.take(self.irrepwise_scaling,idx_l,axis=-2)
         
-    def __call__(self, feature_array: Float[Array, '... 1 max_ell nfeatures']):
+    def __call__(self, feature_array: Float[Array, '... 1 (max_ell+1)**2 nfeatures']):
         return self.irrepwise_scaling*feature_array
