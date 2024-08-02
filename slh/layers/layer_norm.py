239dfa14816f2e30ef5b99ae2ee9f2f877f60610
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-08-02 21:48:17 +0000

    Initialize learnable scaling parameter and define __call__

--- slh/layers/layer_norm.py
+++ slh/layers/layer_norm.py
@@ -5,8 +5,15 @@ import jax.numpy as jnp
 from jaxtyping import Array, Float, Int
 
 class LayerNorm(nn.module):
+    nfeatures: Int
+    max_ell: Int
 
     def setup(self):
+        # 1 x P x L x F
+        self.irrepwise_scaling = self.param("irrepwise_scaling",
+                                            nn.initializers.constant(1.0),
+                                            shape = (1, 1, self.max_ell+1, self.nfeatures)
+                                           )
         
-
-    def __call__(self):
+    def __call__(self, feature_array: Float[Array, '... 1 max_ell nfeatures']):
+        return self.irrepwise_scaling*feature_array
