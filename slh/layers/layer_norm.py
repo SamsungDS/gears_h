1899aae745c0c3deb217b0c5aec7d22406970afc
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-08-05 15:51:23 +0000

    Remove need to pass shape to layernorm

--- slh/layers/layer_norm.py
+++ slh/layers/layer_norm.py
@@ -4,16 +4,15 @@ import jax.numpy as jnp
 from jaxtyping import Array, Float, Int
 
 class LayerNorm(nn.module):
-    shape: e3x.nn.features.Shape
     
     @nn.compact
     def __call__(self, feature_array: Float[Array, '... 1 (max_ell+1)**2 nfeatures']):
-        max_ell = e3x.nn.features._extract_max_degree_and_check_shape(self.shape)
-        nfeatures = self.shape[-1]
+        max_ell = e3x.nn.features._extract_max_degree_and_check_shape(feature_array.shape)
+        nfeatures = feature_array.shape[-1]
         idx_l = e3x.nn.modules._duplication_indices_for_max_degree(max_ell)
         irrepwise_scaling = self.param("irrepwise_scaling",
                                        nn.initializers.constant(1.0),
-                                       shape = (1, 1, max_ell+1, nfeatures)
+                                       shape = (1, feature_array.shape[-3], max_ell+1, nfeatures)
                                       )
         irrepwise_scaling = jnp.take(irrepwise_scaling,idx_l,axis=-2)
         # normalization
