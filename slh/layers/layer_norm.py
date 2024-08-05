8f38053e98be6de2188c3eb3c5505f62f7498bed
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-08-05 15:21:59 +0000

    Add feature array normalization to __call__

--- slh/layers/layer_norm.py
+++ slh/layers/layer_norm.py
@@ -10,13 +10,22 @@ class LayerNorm(nn.module):
     def setup(self):
         self.max_ell = e3x.nn.features._extract_max_degree_and_check_shape(self.shape)
         self.nfeatures = self.shape[-1]
-        idx_l = e3x.nn.modules._duplication_indices_for_max_degree(self.max_ell)
+        self.idx_l = e3x.nn.modules._duplication_indices_for_max_degree(self.max_ell)
         # 1 x P x L x F
         self.irrepwise_scaling = self.param("irrepwise_scaling",
                                             nn.initializers.constant(1.0),
                                             shape = (1, 1, self.max_ell+1, self.nfeatures)
                                            )
-        self.irrepwise_scaling = jnp.take(self.irrepwise_scaling,idx_l,axis=-2)
+        self.irrepwise_scaling = jnp.take(self.irrepwise_scaling,self.idx_l,axis=-2)
+        # TODO how and where to do the normalization--do we do it here in the setup once, or every time we call?
         
     def __call__(self, feature_array: Float[Array, '... 1 (max_ell+1)**2 nfeatures']):
-        return self.irrepwise_scaling*feature_array
+        # normalization
+        normalized_feature_array = jnp.zeros(self.shape)
+        for i in range(self.max_ell+1):
+            indices = jnp.where(self.idx_l == i)
+            feature_array_slice = feature_array[...,indices,:]
+            normalized_feature_array_slice = e3x.ops.normalize(feature_array_slice, axis=-2)
+            normalized_feature_array = normalized_feature_array.at[...,indices,:].set(normalized_feature_array_slice)
+        
+        return self.irrepwise_scaling*normalized_feature_array
