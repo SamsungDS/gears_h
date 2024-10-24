8863ae7f6dd2d10383c22ba13ca367b403658f1c
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-10-24 20:30:32 +0000

    MSE, RMSE, and returned variables

--- slh/train/loss.py
+++ slh/train/loss.py
@@ -76,3 +76,29 @@ def irrep_scaled_loss(h_irreps_off_diagonal_predicted,
     normalized_off_diagonal_error = _normalize_error(off_diagonal_error, batch_labels['h_irreps_off_diagonal'])
     on_diagonal_error = h_irreps_on_diagonal_predicted - batch_labels['h_irreps_on_diagonal']
     normalized_on_diagonal_error = _normalize_error(on_diagonal_error, batch_labels['h_irreps_on_diagonal'])
+
+    off_diagonal_mse = jnp.mean(optax.squared_error(normalized_off_diagonal_error),
+                                where=batch_labels["mask_off_diagonal"])
+    on_diagonal_mse = jnp.mean(optax.squared_error(normalized_on_diagonal_error),
+                                where=batch_labels["mask_on_diagonal"])
+    
+    off_diagonal_rmse = jnp.sqrt(off_diagonal_mse)
+    on_diagonal_rmse = jnp.sqrt(on_diagonal_mse)
+
+    weighted_mse = (on_diagonal_weight*on_diagonal_mse + \
+                    off_diagonal_weight*off_diagonal_mse ) / loss_weight_sum
+    weighted_rmse = (on_diagonal_weight*on_diagonal_rmse + \
+                     off_diagonal_weight*off_diagonal_rmse ) / loss_weight_sum
+    weighted_loss = (mse_weight*weighted_mse + rmse_weight*weighted_rmse)/(mse_weight+rmse_weight)
+
+    on_diagonal_mae = jnp.mean(jnp.abs(h_irreps_on_diagonal_predicted - batch_labels["h_irreps_on_diagonal"]),
+                               where=batch_labels["mask_on_diagonal"])
+    off_diagonal_mae = jnp.mean(jnp.abs(h_irreps_off_diagonal_predicted - batch_labels["h_irreps_off_diagonal"]),
+                                where=batch_labels["mask_off_diagonal"])
+    weighted_mean_mae = (on_diagonal_weight*on_diagonal_mae + \
+                         off_diagonal_weight*off_diagonal_mae ) / loss_weight_sum
+    
+    return (loss_multiplier * weighted_loss,
+            weighted_mean_mae,
+            off_diagonal_mae,
+            on_diagonal_mae)
