fc0a71ee8d83c5bf1a3936de22aa19c9ae413055
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-10-24 20:25:01 +0000

    Add _normalize_error helper function and first part of irrep_scaled_loss

--- slh/train/loss.py
+++ slh/train/loss.py
@@ -1,4 +1,8 @@
+from typing import Union
+
+import e3x
 import jax.numpy as jnp
+from jaxtyping import Array, Float
 import optax
 
 def weighted_mse_and_rmse(h_irreps_off_diagonal_predicted,
@@ -37,3 +41,38 @@ def weighted_mse_and_rmse(h_irreps_off_diagonal_predicted,
             weighted_mean_mae,
             off_diagonal_mae,
             on_diagonal_mae)
+
+def _normalize_error(error_array: Union[Float[Array, '... 1 (max_ell+1)**2 nfeatures'],
+                                        Float[Array, '... 2 (max_ell+1)**2 nfeatures']], 
+                     reference_array: Union[Float[Array, '... 1 (max_ell+1)**2 nfeatures'],
+                                            Float[Array, '... 2 (max_ell+1)**2 nfeatures']]):
+    max_ell = e3x.nn.features._extract_max_degree_and_check_shape(reference_array.shape)
+    normalized_error = jnp.zeros(error_array.shape)
+    for ell in range(max_ell + 1):
+        error_array_slice = error_array[..., ell**2 : (ell + 1)**2 ,:]
+        reference_array_slice = reference_array[..., ell**2 : (ell + 1)**2 ,:]
+        reference_array_slice_norm = e3x.ops.norm(reference_array_slice, axis=-2, keepdims=True)
+        # Replace small values in the norm with 1 where they are small enough to make things numerically unstable
+        reference_array_slice_norm = jnp.where(reference_array_slice_norm*reference_array_slice_norm > jnp.finfo(error_array.dtype).tiny,
+                                               reference_array_slice_norm,
+                                               1)
+        normalized_error = normalized_error.at[..., ell**2 : (ell + 1)**2 ,:].set(error_array_slice / reference_array_slice_norm)
+    
+    return normalized_error
+
+def irrep_scaled_loss(h_irreps_off_diagonal_predicted,
+                      h_irreps_on_diagonal_predicted, 
+                      batch_labels,
+                      loss_parameters
+                     ):
+    off_diagonal_weight = loss_parameters['off_diagonal_weight']
+    on_diagonal_weight = loss_parameters['on_diagonal_weight']
+    mse_weight = loss_parameters['mse_weight']
+    rmse_weight = loss_parameters['rmse_weight']
+    loss_multiplier = loss_parameters['loss_multiplier']
+    loss_weight_sum = off_diagonal_weight + on_diagonal_weight
+
+    off_diagonal_error = h_irreps_off_diagonal_predicted - batch_labels['h_irreps_off_diagonal']
+    normalized_off_diagonal_error = _normalize_error(off_diagonal_error, batch_labels['h_irreps_off_diagonal'])
+    on_diagonal_error = h_irreps_on_diagonal_predicted - batch_labels['h_irreps_on_diagonal']
+    normalized_on_diagonal_error = _normalize_error(on_diagonal_error, batch_labels['h_irreps_on_diagonal'])
