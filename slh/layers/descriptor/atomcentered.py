6d2db4d7be32a178d9a16b4fb20ef0bf6bbacd1d
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2024-11-19 16:27:15 +0000

    Fix SA instantiation in new STDSA

--- slh/layers/descriptor/atomcentered.py
+++ slh/layers/descriptor/atomcentered.py
@@ -212,7 +212,7 @@ class SlightlyDifferentShallowTDSAAtomCenteredDescriptor(nn.Module):
             param_dtype=jnp.float32,
         )
 
-        self.mp_block: e3x.nn.SelfAttention = e3x.nn.SelfAttention(
+        self.mp_block: e3x.nn.SelfAttention = partial(e3x.nn.SelfAttention,
             max_degree=self.mp_degree,
             cartesian_order=False,
             use_basis_bias=False,
@@ -275,7 +275,7 @@ class SlightlyDifferentShallowTDSAAtomCenteredDescriptor(nn.Module):
         y = e3x.ops.indexed_sum(y, dst_idx=idx_i, num_segments=len(atomic_numbers))
 
         for _ in range(self.mp_steps):
-            y = self.mp_block(
+            y = self.mp_block()(
                 inputs=y,
                 basis=self.mp_basis(neighbour_displacements),
                 src_idx=idx_j,
