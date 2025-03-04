8c7eddb455579ff581c0f0c15e1eaa43bc2f3b63
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2025-03-04 19:58:49 +0000

    Add logging and type hints to existing functions.

--- slh/infer/infer.py
+++ slh/infer/infer.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 
@@ -5,7 +6,12 @@ from ase.io import read
 import numpy as np
 from slhtools.utils import get_neighbourlist_ijD
 
-def process_structure_for_inference(structure_path, cutoff):
+from slh.train.run import setup_logging
+
+log = logging.getLogger(__name__)
+
+def process_structure_for_inference(structure_path: Path, 
+                                    cutoff: float):
 
     atoms = read(structure_path)
     numbers = atoms.get_atomic_numbers()
@@ -13,3 +19,10 @@ def process_structure_for_inference(structure_path, cutoff):
     bonds = np.arange(len(D))
     
     return numbers[None,...], ij[None,...], D[None,...], bonds[None,...]
+
+def infer(config_path: Path| str, 
+          structure_path: Path | str):
+
+    setup_logging(Path.cwd() / "inference.log", "info")
+
+    os.environ["JAX_PLATFORMS"] = "cpu"
