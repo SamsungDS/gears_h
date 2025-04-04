a1318a223a934fe823ee22121272a91be064b212
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2025-04-04 20:41:06 +0000

    Update model initialization for new inputs.

--- slh/train/checkpoints.py
+++ slh/train/checkpoints.py
@@ -61,8 +61,10 @@ def create_params(model, rng_key, sample_input: dict, n_models: int):
         params = model.init(
             model_rng[0],
             jnp.array(sample_input["numbers"]),
-            jnp.array(sample_input["idx_ij"]),
-            jnp.array(sample_input["idx_D"]),
+            jnp.array(sample_input["bc_ij"]),
+            jnp.array(sample_input["bc_D"]),
+            jnp.array(sample_input["ac_ij"]),
+            jnp.array(sample_input["ac_D"]),
             jnp.array(sample_input["idx_bonds"])
         )
         # params = model.init(jax.random.PRNGKey(245), *sample_input)
