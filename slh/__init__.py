ea58a749fad41e4247da4c0fcc3ea2f6e55e50d2
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2025-05-08 20:10:46 +0000

    Default to maximum precision for matrix multiplication unless the user provides the corresponding jax environment variable.

--- slh/__init__.py
+++ slh/__init__.py
@@ -11,3 +11,5 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 e3x.Config.set_cartesian_order(False)
 e3x.Config.set_normalization("orthonormal")
 # jax.config.update("jax_enable_x64", True)
+if "JAX_DEFAULT_MATMUL_PRECISION" not in os.environ:
+    jax.config.update("jax_default_matmul_precision", "highest")
